58a5c95564b840bf40af8014311ed87c00820446
Vladimír Čičmanec <vladimir.cicmanec@notum.cz>
2025-08-27 10:05:02 +0200

    feat: card deck condition

---
+++ apps/strapi/src/rbac/conditions/card-deck.ts
@@ -0,0 +1,29 @@
+import { Core } from "@strapi/strapi"
+
+import { isUserAdmin } from "./util/check-roles"
+import { RbacConditionUser } from "./util/types"
+
+export const createCardDeckAccessCondition = (strapi: Core.Strapi) => ({
+  displayName: "Can access card deck",
+  name: "can-access-card-deck",
+  plugin: "admin",
+  async handler(user: RbacConditionUser) {
+    if (user.permission.subject !== "api::card-deck.card-deck") {
+      throw new Error("Can access card deck condition added to incorrect type")
+    }
+
+    const isAdmin = isUserAdmin(user.roles)
+
+    if (isAdmin) {
+      return true
+    }
+
+    const userSchool = await strapi.documents("api::school.school").findFirst({
+      filters: {
+        administrators: { id: user.id },
+      },
+    })
+
+    return { "school.id": userSchool.id }
+  },
+})
