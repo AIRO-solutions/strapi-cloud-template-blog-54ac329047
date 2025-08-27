cefc9d12c2d0fa4d6d8f5e18b83660f36f36d919
Vladimír Čičmanec <vladimir.cicmanec@notum.cz>
2025-08-27 10:19:11 +0200

    refactor: move types

--- apps/strapi/src/rbac/conditions/util/types.ts
+++ apps/strapi/src/rbac/conditions/util/types.ts
@@ -1,6 +1,13 @@
 import type { UID } from "@strapi/strapi"
 
+export type RbacRole = {
+  id: number
+  code: "strapi-admin"
+}
+
 export type RbacConditionUser = {
+  id: number
+  roles: RbacRole[]
   permission: {
     actionParameters: object
     conditions: string[]
